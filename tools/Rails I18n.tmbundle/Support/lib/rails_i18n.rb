7202ef8f84e27defa475c9131ba0b4555ab22ad9
Kieran Pilkington <kieran@katipo.co.nz>
2009-02-25 07:33:22 +0800

    enhancement: moving the bulk of the command into Support/lib/rails_i18n.rb as it's easier to maintain in it's new class form. Adding checks to stop various errors like nil.gsub and nil.split
    Signed-off-by: Sven Fuchs <svenfuchs@artweb-design.de>

--- tools/Rails I18n.tmbundle/Support/lib/rails_i18n.rb
+++ tools/Rails I18n.tmbundle/Support/lib/rails_i18n.rb
@@ -1,3 +1,4 @@
+require ENV['TM_SUPPORT_PATH'] + '/lib/ui.rb'
 require File.join(File.dirname(__FILE__), 'dictionary')
 require File.join(File.dirname(__FILE__), 'yaml_waml')
 
@@ -17,4 +18,62 @@ class Hash
       self[key].set keys, value
     end
   end
+end
+
+class Translate
+
+  @@project_dir = ENV['TM_PROJECT_DIRECTORY']
+  @@t_path_plain = File.join(@@project_dir, 'log', 'translations')
+  @@t_path_yaml = File.join(@@project_dir, 'log', 'translations.yml')
+  @@original_text = ENV['TM_SELECTED_TEXT']
+
+  def self.execute
+    return if @@original_text.nil?
+
+    key = self.get_key
+    if key.nil? || key.gsub(/\s/, '').empty?
+      print @@original_text
+      return
+    end
+
+    translation = ENV['TM_SELECTED_TEXT'].gsub(/^\s*("|')|("|')\s*$/, '')
+    self.add_plain_translation(key, translation)
+    self.add_yaml_translation(key, translation)
+
+    case self.get_type
+    when 'html'
+      print "<%=#{self.translation_method} '#{key}' %>"
+    when 'string'
+      print "\#{#{self.translation_method}('#{key}')}"
+    else
+      print "#{self.translation_method}('#{key}')"
+    end
+  end
+
+  def self.get_key
+    TextMate::UI.request_string :title => 'Key', :prompt => 'Key'
+  end
+
+  def self.get_type
+    TextMate::UI.request_string :title => 'Type', :prompt => 'html, string, or ruby'
+  end
+
+  def self.add_plain_translation(key, text)
+    log_file = File.open(@@t_path_plain, 'a+')
+    log_file.puts "#{key}: #{text}"
+  end
+
+  def self.add_yaml_translation(key, text)
+    keys = ['en'] + key.split('.')
+    data = { 'en' => {} }
+    data.set keys, text
+    data = data.deep_merge YAML.load(File.open(@@t_path_yaml, 'r') { |f| f.read }) if File.exists?(@@t_path_yaml)
+    File.open(@@t_path_yaml, 'w+') { |f| f.write YAML.dump(data) }
+  end
+
+  def self.translation_method
+    current_file = ENV['TM_FILEPATH'].gsub(@@project_dir, '')
+    translate_cmd = (current_file =~ /^\/app\/(controllers|helpers|views)\//) ? 't' : 'I18n.t'
+  end
+
 end
