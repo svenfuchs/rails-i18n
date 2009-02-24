a47d689555bbac4af6338ce063589dc89ae553c6
Kieran Pilkington <kieran@katipo.co.nz>
2009-02-25 06:46:58 +0800

    enhancement: detect which file we are editing and use the appropriate method in that file (t for controllers, helpers, and views, I18n.t elsewhere, like models and libs). Moved the hash class declarations to Support/lib/rails-i18n.rb file.
    Signed-off-by: Sven Fuchs <svenfuchs@artweb-design.de>

--- tools/Rails I18n.tmbundle/Support/lib/rails_i18n.rb
+++ tools/Rails I18n.tmbundle/Support/lib/rails_i18n.rb
@@ -1,2 +1,20 @@
 require File.join(File.dirname(__FILE__), 'dictionary')
 require File.join(File.dirname(__FILE__), 'yaml_waml')
+
+class Hash
+  def deep_merge(other)
+    # deep_merge by Stefan Rusterholz, see http://www.ruby-forum.com/topic/142809
+    merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
+    merge(other, &merger)
+  end
+
+  def set(keys, value)
+    key = keys.shift
+    if keys.empty?
+      self[key] = value
+    else
+      self[key] ||= {}
+      self[key].set keys, value
+    end
+  end
+end
