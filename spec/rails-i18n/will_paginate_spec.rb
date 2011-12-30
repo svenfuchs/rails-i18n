10505e694129c77ba8dc08bc78212d1673a58548
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-12-30 12:00:03 +0900

    Fill in missing items (will_paginate/{en,ja}.yml)

--- spec/rails-i18n/will_paginate_spec.rb
+++ spec/rails-i18n/will_paginate_spec.rb
@@ -1,9 +1,7 @@
-# coding: utf-8
-
 require 'spec_helper'
 
 describe "will_paginate" do
   it "should give translations for will_paginate" do
-    I18n.t("will_paginate.next_label").should == "next →"
+    I18n.t("will_paginate.next_label").should == "Next &#8594;"
   end
 end
