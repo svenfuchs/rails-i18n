1266fc48a566cfed6735b2682a9418fb3b0d3c10
Christopher Dell <chris@tigrish.com>
2011-12-31 12:31:05 +0100

    Add in the i18n-spec gem and specs

--- spec/rails-i18n/will_paginate_spec.rb
+++ spec/rails-i18n/will_paginate_spec.rb
@@ -5,3 +5,10 @@ describe "will_paginate" do
     I18n.t("will_paginate.next_label").should == "Next &#8594;"
   end
 end
+
+Dir.glob('will_paginate/*.yml') do |locale_file|
+  describe "a will_paginate locale file" do
+    it_behaves_like 'a valid locale file', locale_file
+    it { locale_file.should be_a_subset_of('will_paginate/en-US.yml') }
+  end
+end
