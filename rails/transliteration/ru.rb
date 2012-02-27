f6e5900dd1d254722c05f47948f1be1d5e331e8e
Nash Bridges <pleasesendmeaspam@gmail.com>
2012-02-27 21:07:58 +0200

    Use Russian transliteration spec from Russian gem

--- rails/transliteration/ru.rb
+++ rails/transliteration/ru.rb
@@ -1,5 +1,5 @@
 # (c) Yaroslav Markin, Julian "julik" Tarkhanov and Co
-# (http://github.com/yaroslav/russian)
+# https://github.com/yaroslav/russian/blob/master/lib/russian/transliteration.rb
 
 module RailsI18n
   module Transliteration
@@ -69,4 +69,4 @@ end
 { :ru => {
     :i18n => {
       :transliterate => {
-        :rule => ::RailsI18n::Transliteration::Russian.rule }}}}
+        :rule => RailsI18n::Transliteration::Russian.rule }}}}
