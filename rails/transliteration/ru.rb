75eaaf6499fd7cf3552b7705fc584308debd8e4f
Nash Bridges <pleasesendmeaspam@gmail.com>
2012-03-02 21:18:24 +0200

    Remove 'constant reassign' warnings

--- rails/transliteration/ru.rb
+++ rails/transliteration/ru.rb
@@ -6,62 +6,79 @@
 module RailsI18n
   module Transliteration
     module Russian
-      LOWER_SINGLE = {
-        "і"=>"i","ґ"=>"g","ё"=>"yo","№"=>"#","є"=>"e",
-        "ї"=>"yi","а"=>"a","б"=>"b",
-        "в"=>"v","г"=>"g","д"=>"d","е"=>"e","ж"=>"zh",
-        "з"=>"z","и"=>"i","й"=>"y","к"=>"k","л"=>"l",
-        "м"=>"m","н"=>"n","о"=>"o","п"=>"p","р"=>"r",
-        "с"=>"s","т"=>"t","у"=>"u","ф"=>"f","х"=>"h",
-        "ц"=>"ts","ч"=>"ch","ш"=>"sh","щ"=>"sch","ъ"=>"'",
-        "ы"=>"y","ь"=>"","э"=>"e","ю"=>"yu","я"=>"ya",
-      }
+      class << self
+        def rule
+          lambda do |string|
+            chars = string.scan(%r{#{multi_keys.join '|'}|\w|.})
 
-      LOWER_MULTI = {
-        "ье"=>"ie",
-        "ьё"=>"ie",
-      }
+            result = ""
 
-      UPPER_SINGLE = {
-        "Ґ"=>"G","Ё"=>"YO","Є"=>"E","Ї"=>"YI","І"=>"I",
-        "А"=>"A","Б"=>"B","В"=>"V","Г"=>"G",
-        "Д"=>"D","Е"=>"E","Ж"=>"ZH","З"=>"Z","И"=>"I",
-        "Й"=>"Y","К"=>"K","Л"=>"L","М"=>"M","Н"=>"N",
-        "О"=>"O","П"=>"P","Р"=>"R","С"=>"S","Т"=>"T",
-        "У"=>"U","Ф"=>"F","Х"=>"H","Ц"=>"TS","Ч"=>"CH",
-        "Ш"=>"SH","Щ"=>"SCH","Ъ"=>"'","Ы"=>"Y","Ь"=>"",
-        "Э"=>"E","Ю"=>"YU","Я"=>"YA",
-      }
-
-      UPPER_MULTI = {
-        "ЬЕ"=>"IE",
-        "ЬЁ"=>"IE",
-      }
-
-      LOWER = (LOWER_SINGLE.merge(LOWER_MULTI)).freeze
-      UPPER = (UPPER_SINGLE.merge(UPPER_MULTI)).freeze
-      MULTI_KEYS = (LOWER_MULTI.merge(UPPER_MULTI)).keys.sort_by {|s| s.length}.reverse.freeze
-
-      def self.rule
-        lambda do |string|
-          chars = string.scan(%r{#{MULTI_KEYS.join '|'}|\w|.})
-
-          result = ""
-
-          chars.each_with_index do |char, index|
-            if UPPER.has_key?(char) && LOWER.has_key?(chars[index+1])
-              # combined case
-              result << UPPER[char].downcase.capitalize
-            elsif UPPER.has_key?(char)
-              result << UPPER[char]
-            elsif LOWER.has_key?(char)
-              result << LOWER[char]
-            else
-              result << char
+            chars.each_with_index do |char, index|
+              if upper.has_key?(char) && lower.has_key?(chars[index+1])
+                # combined case
+                result << upper[char].downcase.capitalize
+              elsif upper.has_key?(char)
+                result << upper[char]
+              elsif lower.has_key?(char)
+                result << lower[char]
+              else
+                result << char
+              end
             end
-          end
 
-          result
+            result
+          end
+        end
+
+        private
+
+        # use instance variables instead of constants to prevent warnings
+        # on re-evaling after I18n.reload!
+
+        def upper
+          @upper ||= begin
+            upper_single = {
+              "Ґ"=>"G","Ё"=>"YO","Є"=>"E","Ї"=>"YI","І"=>"I",
+              "А"=>"A","Б"=>"B","В"=>"V","Г"=>"G",
+              "Д"=>"D","Е"=>"E","Ж"=>"ZH","З"=>"Z","И"=>"I",
+              "Й"=>"Y","К"=>"K","Л"=>"L","М"=>"M","Н"=>"N",
+              "О"=>"O","П"=>"P","Р"=>"R","С"=>"S","Т"=>"T",
+              "У"=>"U","Ф"=>"F","Х"=>"H","Ц"=>"TS","Ч"=>"CH",
+              "Ш"=>"SH","Щ"=>"SCH","Ъ"=>"'","Ы"=>"Y","Ь"=>"",
+              "Э"=>"E","Ю"=>"YU","Я"=>"YA",
+            }
+
+            (upper_single.merge(upper_multi)).freeze
+          end
+        end
+
+        def lower
+          @lower ||= begin
+            lower_single = {
+              "і"=>"i","ґ"=>"g","ё"=>"yo","№"=>"#","є"=>"e",
+              "ї"=>"yi","а"=>"a","б"=>"b",
+              "в"=>"v","г"=>"g","д"=>"d","е"=>"e","ж"=>"zh",
+              "з"=>"z","и"=>"i","й"=>"y","к"=>"k","л"=>"l",
+              "м"=>"m","н"=>"n","о"=>"o","п"=>"p","р"=>"r",
+              "с"=>"s","т"=>"t","у"=>"u","ф"=>"f","х"=>"h",
+              "ц"=>"ts","ч"=>"ch","ш"=>"sh","щ"=>"sch","ъ"=>"'",
+              "ы"=>"y","ь"=>"","э"=>"e","ю"=>"yu","я"=>"ya",
+            }
+
+            (lower_single.merge(lower_multi)).freeze
+          end
+        end
+
+        def upper_multi
+          @upper_multi ||= { "ЬЕ"=>"IE", "ЬЁ"=>"IE" }
+        end
+
+        def lower_multi
+          @lower_multi ||= { "ье"=>"ie", "ьё"=>"ie" }
+        end
+
+        def multi_keys
+          @multi_keys ||= (lower_multi.merge(upper_multi)).keys.sort_by {|s| s.length}.reverse.freeze
         end
       end
     end
