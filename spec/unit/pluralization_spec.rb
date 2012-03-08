87ef1a2a132db16ea28c067228bbd95734e59eca
Garami Gábor <hron@hron.me>
2012-03-08 19:45:17 +0100

    Fixing specs against Hungarian language

--- spec/unit/pluralization_spec.rb
+++ spec/unit/pluralization_spec.rb
@@ -8,6 +8,7 @@ require 'spec_helper'
 require 'support/pluralization_file'
 
 require 'unit/pluralization/ordinary'
+require 'unit/pluralization/one'
 require 'unit/pluralization/other'
 require 'unit/pluralization/one_with_zero_other'
 require 'unit/pluralization/one_upto_two_other'
@@ -253,7 +254,7 @@ describe 'Pluralization rule for' do
 
   describe 'Hungarian', :locale => :hu do
     it_behaves_like 'an ordinary pluralization rule'
-    it_behaves_like 'other form language'
+    it_behaves_like 'one form language'
   end
 
   describe 'Igbo', :locale => :ig do
