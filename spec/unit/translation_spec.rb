065da9c66a54bcb5e52f473a244321fff3c61344
Christopher Dell <chris@tigrish.com>
2012-09-18 10:58:19 +0200

    Specs compare translations files to :en and not :en-US

--- spec/unit/translation_spec.rb
+++ spec/unit/translation_spec.rb
@@ -5,6 +5,6 @@ require 'spec_helper'
 Dir.glob('rails/locale/*.yml') do |locale_file|
   describe "a rails-i18n #{locale_file} locale file" do
     it_behaves_like 'a valid locale file', locale_file
-    it { locale_file.should be_a_subset_of('rails/locale/en-US.yml') }
+    it { locale_file.should be_a_subset_of('rails/locale/en.yml') }
   end
 end
