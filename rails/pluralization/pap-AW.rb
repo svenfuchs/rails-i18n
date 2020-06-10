efdf7dc5ffec0c776a3d262399801eba41dc4b8c
Ace Suares <ace@suares.com>
2020-06-10 07:48:36 -0400

    Papiamento has Other, not OneOther!

--- rails/pluralization/pap-AW.rb
+++ rails/pluralization/pap-AW.rb
@@ -1,3 +1,3 @@
-require 'rails_i18n/common_pluralizations/one_other'
+require 'rails_i18n/common_pluralizations/other'
 
-::RailsI18n::Pluralization::OneOther.with_locale(:'pap-AW')
+::RailsI18n::Pluralization::Other.with_locale(:'pap-AW')
