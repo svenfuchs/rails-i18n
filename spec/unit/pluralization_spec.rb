2bbc8c7e7faf164b0650aa449f91998d813d0036
Grey Moore <grey@controlshiftlabs.com>
2021-03-08 11:23:42 -0500

    update specs for serbian pluralization

--- spec/unit/pluralization_spec.rb
+++ spec/unit/pluralization_spec.rb
@@ -656,7 +656,29 @@ describe 'Pluralization rule for' do
 
   describe 'Serbian', :locale => :sr do
     it_behaves_like 'an ordinary pluralization rule'
-    it_behaves_like 'East Slavic'
+
+    it 'has "one", "few", and "other" plural keys' do
+      plural_keys.size.should == 3
+      plural_keys.should include(:one, :few, :other)
+    end
+
+    [0.1, 0.31, 1, 21, 31, 41, 51, 61, 81, 101, 1031].each do |count|
+      it "detects that #{count} in category 'one'" do
+        rule.call(count).should == :one
+      end
+    end
+
+    [0.3, 0.42, 2, 3, 4, 22, 23, 24, 102, 1034].each do |count|
+      it "detects that #{count} in category 'few'" do
+        rule.call(count).should == :few
+      end
+    end
+
+    [0.11, 0.13, 56.78, 11, 12, 13, 14, 15, 27, 100, 1012].each do |count|
+      it "detects that #{count} in category 'other'" do
+        rule.call(count).should == :other
+      end
+    end
   end
 
   describe 'Serbo-Croatian', :locale => :sh do
