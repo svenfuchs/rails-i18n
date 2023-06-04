30a8745785cc23c2929f5bd59c5cfd1902b74670
Jose Luis Duran <jlduran@users.noreply.github.com>
2023-06-04 15:19:22 +0000

    Update rails update script

--- rails/script/update.rb
+++ rails/script/update.rb
@@ -4,11 +4,11 @@ rails_locale_dir = File.expand_path(File.join(curr_dir, "..", "rails"))
 puts "Fetching latest Rails locale files to #{rails_locale_dir}"
 
 exec %(
-  curl -Lo '#{rails_locale_dir}/action_view.yml' https://raw.github.com/rails/rails/master/actionview/lib/action_view/locale/en.yml
+  curl -Lo '#{rails_locale_dir}/action_view.yml' https://raw.github.com/rails/rails/main/actionview/lib/action_view/locale/en.yml
 
-  curl -Lo '#{rails_locale_dir}/active_model.yml' https://raw.github.com/rails/rails/master/activemodel/lib/active_model/locale/en.yml
+  curl -Lo '#{rails_locale_dir}/active_model.yml' https://raw.github.com/rails/rails/main/activemodel/lib/active_model/locale/en.yml
 
-  curl -Lo '#{rails_locale_dir}/active_record.yml' https://raw.github.com/rails/rails/master/activerecord/lib/active_record/locale/en.yml
+  curl -Lo '#{rails_locale_dir}/active_record.yml' https://raw.github.com/rails/rails/main/activerecord/lib/active_record/locale/en.yml
 
-  curl -Lo '#{rails_locale_dir}/active_support.yml' https://raw.github.com/rails/rails/master/activesupport/lib/active_support/locale/en.yml
+  curl -Lo '#{rails_locale_dir}/active_support.yml' https://raw.github.com/rails/rails/main/activesupport/lib/active_support/locale/en.yml
 )
