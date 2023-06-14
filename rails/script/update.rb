d6a2a8834ca8e92e758c8090177633928c871042
Jose Luis Duran <jlduran@users.noreply.github.com>
2023-06-14 20:31:15 +0000

    Fix Rails' update script branch
    We should track the latest Rails release, not the main branch.
    
    Fix 30a8745 by pointing the update script to the proper Rails branch.
    
    Perhaps changing to a git model that more closely mimics Rails' is a
    better approach long-term.  In the meantime, keep bumping the branch in
    the update script as Rails releases are published.

--- rails/script/update.rb
+++ rails/script/update.rb
@@ -4,11 +4,11 @@ rails_locale_dir = File.expand_path(File.join(curr_dir, "..", "rails"))
 puts "Fetching latest Rails locale files to #{rails_locale_dir}"
 
 exec %(
-  curl -Lo '#{rails_locale_dir}/action_view.yml' https://raw.github.com/rails/rails/main/actionview/lib/action_view/locale/en.yml
+  curl -Lo '#{rails_locale_dir}/action_view.yml' https://raw.githubusercontent.com/rails/rails/7-0-stable/actionview/lib/action_view/locale/en.yml
 
-  curl -Lo '#{rails_locale_dir}/active_model.yml' https://raw.github.com/rails/rails/main/activemodel/lib/active_model/locale/en.yml
+  curl -Lo '#{rails_locale_dir}/active_model.yml' https://raw.githubusercontent.com/rails/rails/7-0-stable/activemodel/lib/active_model/locale/en.yml
 
-  curl -Lo '#{rails_locale_dir}/active_record.yml' https://raw.github.com/rails/rails/main/activerecord/lib/active_record/locale/en.yml
+  curl -Lo '#{rails_locale_dir}/active_record.yml' https://raw.githubusercontent.com/rails/rails/7-0-stable/activerecord/lib/active_record/locale/en.yml
 
-  curl -Lo '#{rails_locale_dir}/active_support.yml' https://raw.github.com/rails/rails/main/activesupport/lib/active_support/locale/en.yml
+  curl -Lo '#{rails_locale_dir}/active_support.yml' https://raw.githubusercontent.com/rails/rails/7-0-stable/activesupport/lib/active_support/locale/en.yml
 )
