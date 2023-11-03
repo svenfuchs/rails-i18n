9fb6e1d414fc39bf6aaac56bb5a946481dd45fb2
Christian Schmidt <github@chsc.dk>
2023-11-03 19:00:25 +0100

    Test

--- spec/unit/ordinals_spec.rb
+++ spec/unit/ordinals_spec.rb
@@ -5,9 +5,10 @@ describe 'Ordinals for' do
   # Mock Rails app in order to trigger the Railtie
   let(:app) { double :app, config: config }
   let(:config) { double :config, eager_load_namespaces: [], i18n: I18n, rails_i18n: RailsI18n }
+  let(:period_locales) { %w[be bs cs da de eo et fa fi hr hu is ka lb lt lv mk nb ne nn pl sk sl sq sr sw tr] }
 
   before do
-    I18n.available_locales = %w[fr en fr-CA fr-CH fr-FR gd]
+    I18n.available_locales = %w[fr en fr-CA fr-CH fr-FR gd]+ period_locales
 
     RailsI18n::Railtie.initializers.each { |init| init.run(app) }
     I18n.backend.reload!
